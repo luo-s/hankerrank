66075792d0adb64085cc8e1ecccc7beeee6327ab
Shu <luo.s@outlook.com>
2024-01-03 12:30:50 -0500

    final commit

--- interview_preparation_kit/dict_and_hashmap/two_strings.js
+++ interview_preparation_kit/dict_and_hashmap/two_strings.js
@@ -10,11 +10,32 @@ A substring may be as small as one character.
 // time complexity: O(n+m)
 // space complexity: O(n)
 function twoStrings(s1, s2) {
-    let set1 = new Set(s1);
-    for (let char of s2) {
-        if (set1.has(char)) {
-            return "YES";
-        }
+  let set1 = new Set(s1);
+  for (let char of s2) {
+    if (set1.has(char)) {
+      return "YES";
     }
-    return "NO";
+  }
+  return "NO";
+}
+
+// different approach
+// time complexity: O(n+m)
+// space complexity: O(26)
+function twoStrings(s1, s2) {
+  var generateKey = (str) => {
+    const key = new Array(26).fill(0);
+    for (let char of str) {
+      key[char.charCodeAt(0) - "a".charCodeAt(0)]++;
+    }
+    return key;
+  };
+  const key1 = generateKey(s1);
+  const key2 = generateKey(s2);
+  for (let i = 0; i < 26; i++) {
+    if (key1[i] > 0 && key2[i] > 0) {
+      return "YES";
+    }
+  }
+  return "NO";
 }
