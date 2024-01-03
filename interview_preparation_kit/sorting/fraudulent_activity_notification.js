6c8a442f93ff3a81e0289c3ef8f09fd12485fe40
Shu <luo.s@outlook.com>
2024-01-03 17:00:00 -0500

    add optimized solution

--- interview_preparation_kit/sorting/fraudulent_activity_notification.js
+++ interview_preparation_kit/sorting/fraudulent_activity_notification.js
@@ -7,12 +7,16 @@ number of days, they send the client a notification about potential fraud.
 Given the number of trailing days d and a client's total daily expenditures for a 
 period of  days, determine the number of times the client will receive a 
 notification over all n days.
+
+1 <= n <= 2 * 10^5
+1 <= d <= n
+0 <= expenditure[i] <= 200
 */
 
 // https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
 
 // brute force
-// time complexity: O(n*d*log(d))
+// time complexity: O(n * d * log(d))
 // space complexity: O(d)
 function activityNotifications(expenditure, d) {
   var getMedian = (arr) => {
@@ -29,4 +33,42 @@ function activityNotifications(expenditure, d) {
 }
 
 // optimized solution
-function activityNotifications(expenditure, d) {}
+// time complexity: O(n * d)
+// space complexity: O(1)
+function activityNotifications(expenditure, d) {
+  let res = 0;
+  // initialize the median array
+  let medianArr = new Array(201).fill(0);
+  for (let i = 0; i < d; i++) {
+    medianArr[expenditure[i]]++;
+  }
+  // get the median
+  let getMedian = () => {
+    if (d % 2 === 1) {
+      let count = 0;
+      for (let i = 0; i < medianArr.length; i++) {
+        count += medianArr[i];
+        if (count > d / 2) return i;
+      }
+    } else {
+      let count = 0;
+      let first = null;
+      let second = null;
+      for (let i = 0; i < medianArr.length; i++) {
+        count += medianArr[i];
+        if (first === null && count >= d / 2) first = i;
+        if (second === null && count >= d / 2 + 1) second = i;
+        if (first !== null && second !== null) return (first + second) / 2;
+      }
+    }
+  };
+  // check the notification
+  for (let i = d; i < expenditure.length; i++) {
+    let median = getMedian();
+    if (expenditure[i] >= median * 2) res++;
+    // update the median array
+    medianArr[expenditure[i - d]]--;
+    medianArr[expenditure[i]]++;
+  }
+  return res;
+}
