410272dd2be3ba3feaed2b153d710c4d0e7e6a4a
Shu <luo.s@outlook.com>
2024-01-03 00:47:10 -0500

    optimized solution

--- interview_preparation_kit/dict_and_hashmap/count_triplets.js
+++ interview_preparation_kit/dict_and_hashmap/count_triplets.js
@@ -10,12 +10,31 @@ such that the elements at those indices are in geometric progression for a given
 // space complexity: O(1)
 function countTriplets(arr, r) {
   let count = 0;
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 2; i++) {
     for (let j = i + 1; j < arr.length - 1; j++) {
       for (let k = j + 1; k < arr.length; k++) {
         if (arr[i] * r === arr[j] && arr[j] * r === arr[k]) {
           count++;
         }
+      }
+    }
+  }
+  return count;
+}
+
+// optimized solution
+// time complexity: O(n^2)
+// space complexity: O(n)
+function countTriplets(arr, r) {
+  let count = 0;
+  let tracker = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] * r === arr[j]) {
+        for (let pair of tracker) {
+          if (i === pair[1]) count++;
+        }
+        tracker.push([i, j]);
       }
     }
   }
