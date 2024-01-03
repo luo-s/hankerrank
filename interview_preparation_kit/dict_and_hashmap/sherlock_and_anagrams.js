461b2bd239e51ea545cd4e6cd0ba8823869d647a
Shu <luo.s@outlook.com>
2024-01-02 20:50:40 -0500

    brute force solution

--- interview_preparation_kit/dict_and_hashmap/sherlock_and_anagrams.js
+++ interview_preparation_kit/dict_and_hashmap/sherlock_and_anagrams.js
@@ -7,19 +7,32 @@ s contains only lowercase letters in the range ascii[a-z].
 
 // https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
+// brute force
+// time complexity: O(n^3)
+// space complexity: O(n)
 function sherlockAndAnagrams(s) {
-    function isAnagram(str1, str2) {
-        if (str1.length !== str2.length) return false;
-        const map = {};
-        for (let i = 0; i < str1.length; i++) {
-            map[str1[i]] = map[str1[i]] ? map[str1[i]] + 1 : 1;
-        }
-        for (let i = 0; i < str2.length; i++) {
-            if (map[str2[i]] === undefined) return false;
-            map[str2[i]]--;
-            if (map[str2[i]] < 0) return false;
-        }
-        return true;
+  function isAnagram(str1, str2) {
+    if (str1.length !== str2.length) return false;
+    const map = {};
+    for (let i = 0; i < str1.length; i++) {
+      map[str1[i]] = map[str1[i]] ? map[str1[i]] + 1 : 1;
     }
-
+    for (let i = 0; i < str2.length; i++) {
+      if (map[str2[i]] === undefined) return false;
+      map[str2[i]]--;
+      if (map[str2[i]] < 0) return false;
+    }
+    return true;
+  }
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 1; j < s.length - i; j++) {
+      for (let k = i + 1; k < s.length - j + 1; k++) {
+        if (isAnagram(s.slice(i, i + j), s.slice(k, k + j))) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
 }
