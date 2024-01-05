5a9ccca380331f518e3adc3c4164756e04016dca
Shu <luo.s@outlook.com>
2024-01-05 16:48:30 -0500

    wip

---
+++ interview_preparation_kit/string_manipulation/special_string_again.js
@@ -0,0 +1,13 @@
+/*
+A string is said to be a special string if either of two conditions is met:
+  1. All of the characters are the same, e.g. aaa.
+  2. All characters except the middle one are the same, e.g. aadaa.
+
+A special substring is any substring of a string which meets one of those 
+criteria. Given a string, determine how many special substrings can be 
+formed from it.
+*/
+
+// https://www.hackerrank.com/challenges/special-palindrome-again/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function substrCount(n, s) {}
