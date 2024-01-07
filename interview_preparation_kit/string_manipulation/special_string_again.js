8b8aa6ff4707d1cff09e52d89bdb25f3d64044a4
Shu <luo.s@outlook.com>
2024-01-06 22:55:36 -0500

    brute force

--- interview_preparation_kit/string_manipulation/special_string_again.js
+++ interview_preparation_kit/string_manipulation/special_string_again.js
@@ -10,4 +10,35 @@ formed from it.
 
 // https://www.hackerrank.com/challenges/special-palindrome-again/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
 
-function substrCount(n, s) {}
+// brute force
+// time complexity: O(n^3)
+// space complexity: O(n)
+function substrCount(n, s) {
+  // special string determination function
+  var isSpecial = function (str) {
+    if (str.length === 1) return true;
+    if (str.length % 2 === 0) {
+      for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[0]) return false;
+      }
+      return true;
+    } else {
+      let mid = Math.floor(str.length / 2);
+      for (let i = 0; i < str.length; i++) {
+        if (i === mid) continue;
+        if (str[i] !== str[0]) return false;
+      }
+      return true;
+    }
+  };
+  // count special substrings
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j <= n; j++) {
+      const str = s.substring(i, j);
+      console.log(i, j, str);
+      if (isSpecial(str)) count++;
+    }
+  }
+  return count;
+}
