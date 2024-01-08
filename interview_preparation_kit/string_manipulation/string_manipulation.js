7e95037235ecde7a55a66b6b967ffa5dc3139093
Shu <luo.s@outlook.com>
2024-01-08 01:23:15 -0500

    wip

---
+++ interview_preparation_kit/string_manipulation/string_manipulation.js
@@ -0,0 +1,12 @@
+/*
+A string is said to be a child of a another string if it can be formed by 
+deleting 0 or more characters from the other string. Letters cannot be 
+rearranged. 
+
+Given two strings of equal length, what's the longest string that can be 
+constructed such that it is a child of both?
+*/
+
+// https://www.hackerrank.com/challenges/common-child/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+
+function commonChild(s1, s2) {}
