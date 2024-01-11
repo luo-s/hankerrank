7e17db64a974d711366ae28f6630a3ff5a824e3b
Shu <luo.s@outlook.com>
2024-01-10 22:46:21 -0500

    wip

---
+++ interview_preparation_kit/search/pairs.js
@@ -0,0 +1,8 @@
+/*
+Given an array of integers and a target value, determine the number of pairs 
+of array elements that have a difference equal to the target value.
+*/
+
+// https://www.hackerrank.com/challenges/pairs/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
+
+function pairs(k, arr) {}
