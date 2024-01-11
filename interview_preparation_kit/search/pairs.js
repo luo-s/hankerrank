f5d376d7c4dbe9f0b6a722a352cc0d55f0233498
Shu <luo.s@outlook.com>
2024-01-10 22:49:35 -0500

    brute force

--- interview_preparation_kit/search/pairs.js
+++ interview_preparation_kit/search/pairs.js
@@ -5,4 +5,17 @@ of array elements that have a difference equal to the target value.
 
 // https://www.hackerrank.com/challenges/pairs/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=search
 
-function pairs(k, arr) {}
+// brute force
+// time complexity O(n^2)
+// space  complexity O(1)
+function pairs(k, arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[j] - arr[i]) === k) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
