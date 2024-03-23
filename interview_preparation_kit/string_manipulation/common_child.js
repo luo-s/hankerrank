4873a3ba8c33d23db1c6e6349b60b5c389b9dffd
Shu <luo.s@outlook.com>
2024-03-23 16:33:39 -0400

    good copy

--- interview_preparation_kit/string_manipulation/common_child.js
+++ interview_preparation_kit/string_manipulation/common_child.js
@@ -15,7 +15,9 @@ constructed such that it is a child of both?
 function commonChild(s1, s2) {
   const m = s1.length;
   const n = s2.length;
-  // Create a 2D array to store the length of common subsequences
+  // let dp[i][j] be the LCS of s[0:i-1] and s2[0:j-1]
+  // if s1[i-1] === s2[j-1], dp[i][j] = dp[i-1][j-1] + 1
+  // else dp[i][j] = max(dp[i-1][j], dp[i][j-1])
   const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
   // Fill the dp array using bottom-up dynamic programming
   for (let i = 1; i <= m; i++) {
@@ -30,13 +32,3 @@ function commonChild(s1, s2) {
   // The length of the LCS is stored in dp[m][n]
   return dp[m][n];
 }
-
-console.log(commonChild("HARRY", "SALLY")); // 2
-console.log(commonChild("AA", "BB")); // 0
-console.log(commonChild("SHINCHAN", "NOHARAAA")); // 3
-console.log(
-  commonChild(
-    "WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS",
-    "FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC"
-  )
-); // 15
