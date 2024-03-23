84117130a014d45e17fd72c8bbe639fb1c3a8120
Shu <luo.s@outlook.com>
2024-03-23 14:52:10 -0400

    good copy

--- interview_preparation_kit/dict_and_hashmap/ransom_note.js
+++ interview_preparation_kit/dict_and_hashmap/ransom_note.js
@@ -6,25 +6,23 @@ the magazine; otherwise, print No.
 
 // https://www.hackerrank.com/challenges/ctci-ransom-note/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 
-// time complexity: O(m+2n)
-// space complexity: O(m+n)
+// time complexity: O(m+n)
+// space complexity: O(m)
 function checkMagazine(magazine, note) {
   if (note.length > magazine.length) {
     console.log("No");
     return;
   }
   const mapMagazine = new Map();
-  const mapNote = new Map();
   for (let word of magazine) {
     mapMagazine.set(word, (mapMagazine.get(word) || 0) + 1);
   }
   for (let word of note) {
-    mapNote.set(word, (mapNote.get(word) || 0) + 1);
-  }
-  for (let [word, count] of mapNote.entries()) {
-    if (!mapMagazine.has(word) || mapMagazine.get(word) < count) {
+    if (!mapMagazine.has(word) || mapMagazine.get(word) === 0) {
       console.log("No");
       return;
+    } else {
+      mapMagazine.set(word, mapMagazine.get(word) - 1);
     }
   }
   console.log("Yes");
