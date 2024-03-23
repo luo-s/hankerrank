ddf318a4cd3a1b62227626850ce0f6474b843f14
Shu <luo.s@outlook.com>
2024-03-23 14:21:22 -0400

    prefix sum

--- interview_preparation_kit/array/array_manipulation.js
+++ interview_preparation_kit/array/array_manipulation.js
@@ -12,7 +12,7 @@ in the array.
 
 // https://www.hackerrank.com/challenges/crush/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
-// brute force
+// brute force -- time limit exceeded
 // time complexity: O(m * n)
 // space complexity: O(n)
 function arrayManipulation(n, queries) {
@@ -27,4 +27,33 @@ function arrayManipulation(n, queries) {
     }
   }
   return Math.max(...arr);
+}
+
+/* optimized solution: prefix sum can unnest the nested loop -> 2 indiviaul loops
+original: arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+queries: [[1, 5, 3], [4, 8, 7], [6, 9, 1]]
+after 1st query:     [3, 0, 0, 0, -3, 0, 0, 0, 0, 0] 
+  prefix sum (arr) = [3, 3, 3, 3, 3, 0, 0, 0, 0, 0]
+after 2nd query:     [3, 0, 0, 7, 0, -3, 0, 0, -7, 0]
+  prefix sum (arr) = [3, 3, 3, 10, 10, 7, 7, 7, 0, 0]
+after 3rd query:     [3, 0, 0, 7, 0, -2, 0, 0, -7, -1] 
+  prefix sum (arr) = [3, 3, 3, 10, 10, 8, 8, 8, 1, 0]
+*/
+// time complexity: O(n + m)
+// sapce complexity: O(n)
+function arrayManipulation(n, queries) {
+  // initialize an array of length n + 1
+  let arr = new Array(n + 1).fill(0),
+    max = 0;
+  // loop through queries, add value to start index, and subtract value from end index
+  for (let [start, end, value] of queries) {
+    arr[start - 1] += value;
+    arr[end] -= value;
+  }
+  let prefixSum = 0;
+  for (let i = 0; i < n; i++) {
+    prefixSum += arr[i];
+    max = Math.max(max, prefixSum);
+  }
+  return max;
 }
